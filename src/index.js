7529ea9f81373b0f48759896ca0d422e0bde73e2
bushmusi <bushera.mestofa@gmail.com>
2022-02-28 22:18:59 +0300

    Ready for review

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import './style.css';
+import Todo from './add-remove.js';
+
+const addRemoveObj = new Todo();
+const { form } = addRemoveObj;
+form.addEventListener('submit', addRemoveObj.addElement);
+addRemoveObj.itemList();
+addRemoveObj.todoInputBox.focus();
+
+const refreshIcon = document.getElementById('refresh-link');
+refreshIcon.addEventListener('click', () => addRemoveObj.itemList());
+
+const multipleDelete = (event) => {
+  const checkedItems = document.querySelectorAll('input[type=checkbox]:checked');
+  const filtIds = [];
+  checkedItems.forEach((item, index) => {
+    const [, id] = item.id.split('-');
+    filtIds.push(+id);
+  });
+  let itemList = [];
+  try {
+    itemList = JSON.parse(localStorage.getItem('todo-list'));
+  } catch (e) {
+    throw new Error(`Error occured: ${e}`);
+  }
+  itemList = itemList.filter((val, index) => filtIds.indexOf(val.index) === -1);
+  itemList.forEach((val, index) => {
+    val.index = index + 1;
+  });
+  localStorage.setItem('todo-list', JSON.stringify(itemList));
+
+  addRemoveObj.itemList();
+};
+
+const clearAllElement = document.getElementById('clear-completed');
+clearAllElement.addEventListener('click', (event) => {
+  multipleDelete(event);
+});
